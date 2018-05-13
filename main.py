9e41fce733adb118f0966264d9c4d34bd8ae9246
Oscar024 <or.carvajal.ortiz@gmail.com>
2018-05-12 19:36:31 -0700

    Cambio de resultado a porcentaje

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import math
 
-edad =60
-sexo = 1 #mujer
-bmi = 32
-sys = 128
-dia = 85
-fuma = 1
-padres = 2
+edad =26
+sexo = 0 #mujer
+bmi = 26.1
+sys = 110
+dia = 75
+fuma = 0
+padres = 0
 agedb = edad*dia
 
 
 
-rirsk = 1-math.exp(-math.exp((math.log(4) - (22.9495 + (-0.1564*edad )+( -0.2029*sexo) + (-0.0339*bmi) + (-0.0593*sys) + (-0.1285*dia) + (-0.1907*fuma) +  (-0.1661*padres) + (0.0016*agedb))/0.8769)))
-
-print(rirsk)
+risk = 1-math.exp(-math.exp((math.log(4) - (22.9495 + (-0.1564*edad )+( -0.2029*sexo) + (-0.0339*bmi) + (-0.0593*sys) + (-0.1285*dia) + (-0.1907*fuma) +  (-0.1661*padres) + (0.0016*agedb))/0.8769)))
+risk = risk*100
+print(risk)
